28a73a6393096530fa02b11a01497959aa35b6a5
DanielGarciaAcebo <danielsante@outlook.es>
2023-02-07 15:32:09 +0100

    table empty

---
+++ src/app/country/country.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+
+import { ForCapitalComponent } from './pages/for-capital/for-capital.component';
+import { ForCountryComponent } from './pages/for-country/for-country.component';
+import { ForRegionComponent } from './pages/for-region/for-region.component';
+import { ViewCountryComponent } from './pages/view-country/view-country.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    ForCapitalComponent,
+    ForCountryComponent,
+    ForRegionComponent,
+    ViewCountryComponent
+  ],
+  exports:[
+    ForCapitalComponent,
+    ForCountryComponent,
+    ForRegionComponent,
+    ViewCountryComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ]
+})
+export class CountryModule { }
