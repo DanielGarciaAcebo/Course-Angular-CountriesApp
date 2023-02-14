7e0c479f440f0fb91831ecdeacbdfc8aa61a665e
DanielGarciaAcebo <danielsante@outlook.es>
2023-02-14 13:08:50 +0100

    finish first part of CountriesApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,47 @@
-import {NgModule} from '@angular/core'
-import {RouterModule, Routes} from "@angular/router";
-import {ForCountryComponent} from "./country/pages/for-country/for-country.component";
-import {ForRegionComponent} from "./country/pages/for-region/for-region.component";
-import {ForCapitalComponent} from "./country/pages/for-capital/for-capital.component";
-import {ViewCountryComponent} from "./country/pages/view-country/view-country.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes =[
-  {
-    path:"",
-    component: ForCountryComponent,
-    pathMatch:"full"
-  },
-  {
-    path:"region",
-    component: ForRegionComponent,
-  },
-  {
-    path:"capital",
-    component: ForCapitalComponent,
-  },
-  {
-    path:"pais/:id",
-    component: ViewCountryComponent,
-  },
-  {
-    path:"**",
-    redirectTo: "",
-  },
-]
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PorPaisComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'region',
+        component: PorRegionComponent
+    },
+    {
+        path: 'capital',
+        component: PorCapitalComponent
+    },
+    {
+        path: 'pais/:id',
+        component: VerPaisComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
+
+
+
 
 @NgModule({
-  imports:[
-    RouterModule.forRoot(routes)
-  ],
-  exports:[
-  RouterModule
-  ],
+    imports: [
+        RouterModule.forRoot( routes )
+    ],
+    exports: [
+        RouterModule
+    ]
 })
+export class AppRoutingModule {}
 
-export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CountryModule } from "./country/country.module";
-import { SharedModule } from "./shared/shared.module";
-
-
 
 @NgModule({
   declarations: [
@@ -17,10 +16,9 @@ import { SharedModule } from "./shared/shared.module";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CountryModule,
-    SharedModule,
+    PaisModule,
+    SharedModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
